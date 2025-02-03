e2869bca93685592b89d447dfbef62fb8f8955e3
Chaitanya Kumar Dewangan <chaitanyakumar1401@gmail.com>
2025-02-03 09:47:43 +0530

    listView builder constructor

--- lib/list_view_example.dart
+++ lib/list_view_example.dart
@@ -1,15 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class ListViewExample extends StatelessWidget{
+
+class ListViewExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<String> items = List.generate(1000, (index) => 'Item ${index+1}');
+    List<String> items = List.generate(1000, (index) => 'Item ${index + 1}');
     return Scaffold(
-      backgroundColor: Colors.green,
-      appBar: AppBar(title: Text('ListView Example', style: TextStyle(color: Colors.white),),),
-      body: ListView(
-        children: items.map((e) => Text(e)).toList(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Contact App', style: TextStyle(color: Colors.white)),
+        backgroundColor: CupertinoColors.activeGreen,
+        actions: [
+          IconButton(icon: Icon(Icons.add, color: Colors.white,), onPressed: (){
+            print('add button');
+          },),
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              // Add search functionality here
+              print('Search button pressed');
+            },
+          ),
+
+          IconButton(
+            icon: Icon(Icons.menu, color: Colors.white),
+            onPressed: () {
+              // Add menu functionality here
+              print('Menu button pressed');
+            },
+          ),
+
+        ],
+      ),
+      body: ListView.builder(
+        
+        itemCount: items.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: ListTile(
+            tileColor: index.isEven? Colors.lightGreen.shade100: Colors.lightGreen.shade200,
+            title: Text('Contact ${index + 1}'),
+            subtitle: Text('This is contact ${index + 1}'),
+          ),
+        ),
       ),
     );
   }
-
 }
